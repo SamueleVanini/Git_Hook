a8a4876e4f3279cc632673f699f61a8006e4e2a2
SamueleVanini <samuele.vanini@live.it>
2017-06-20 15:47:13 +0200

    Aggiornamento percorsi cartelle

--- config_script/git_template_conf.cmd
+++ config_script/git_template_conf.cmd
@@ -3,12 +3,10 @@ set @path_from=%~dp0
 set @path_from=%@path_from:\config_script=\hooks%
 set @path_from=%@path_from%pre-commit
 set @path_to=%~dp0
-set @path_to=%@path_to:\Git_Hook\config_script=\.git\hooks%
+set @path_to=%@path_to:\Git_Hook-master\config_script=\.git\hooks%
 set @file_orig=%@path_to%pre-commit
 for /R %@path_to% %%A in (*) do (
     if  "%%~nxA" EQU "pre-commit" (
-        echo dentro if
-        echo %@file_orig%
         copy %@file_orig%+%@path_from% %@path_to%
         exit(0)
     )
